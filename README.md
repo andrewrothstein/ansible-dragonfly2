d3acc3d01ec7fdef64210856f12c6d9fbdc4e1ba
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-12-12 20:13:02 -0500

    dragonfly2:2.0.1

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.dragonfly2
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-dragonfly2/actions/workflows/build.yml/badge.svg)
+
+Installs [Dragonfly2](https://github.com/dragonflyoss/Dragonfly2).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.dragonfly2
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+#set -x
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/dragonflyoss
+APP=Dragonfly2
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}-${arch}"
+    local file="${APP}-${ver}-${platform}.${archive_type}"
+    local url=$MIRROR/$APP/releases/download/v$ver/$file
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local checksums="${APP}_${ver}_checksums.txt"
+    # https://github.com/dragonflyoss/Dragonfly2/releases/download/v2.0.1/checksums.txt
+    local url=$MIRROR/$APP/releases/download/v$ver/checksums.txt
+    local lchecksums="${DIR}/${checksums}"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums linux amd64
+}
+
+dl_ver ${1:-2.0.1}
