3d778a7ee33fe8f8cc3de16e923cc6c5eb67f0b0
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-11-25 10:59:31 -0500

    dragonfly:2.0.7 drop alpine, prior dragonfly2 versions

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
-#set -x
 set -e
 DIR=~/Downloads
-MIRROR=https://github.com/dragonflyoss
-APP=Dragonfly2
+MIRROR=https://github.com/dragonflyoss/Dragonfly2/releases/download
 
 dl() {
     local ver=$1
@@ -12,28 +10,29 @@ dl() {
     local arch=$4
     local archive_type=${5:-tar.gz}
     local platform="${os}-${arch}"
-    local file="${APP}-${ver}-${platform}.${archive_type}"
-    local url=$MIRROR/$APP/releases/download/v$ver/$file
+    local file="dragonfly-${ver}-${platform}.${archive_type}"
+    local url=$MIRROR/v$ver/$file
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
-    local checksums="${APP}_${ver}_checksums.txt"
     # https://github.com/dragonflyoss/Dragonfly2/releases/download/v2.0.1/checksums.txt
-    local url=$MIRROR/$APP/releases/download/v$ver/checksums.txt
-    local lchecksums="${DIR}/${checksums}"
+    local url=$MIRROR/v$ver/checksums.txt
+    local lchecksums="${DIR}/dragonfly2_${ver}_checksums.txt"
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
     printf "  '%s':\n" $ver
 
     dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums darwin arm64
     dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm64
 }
 
-dl_ver ${1:-2.0.3}
+dl_ver ${1:-2.0.7}
